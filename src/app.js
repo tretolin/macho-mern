631b39a40cf9b30bd3a60979cc820d32f2c0d1f0
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2023-10-23 16:28:01 -0600

    Added json configuration and routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import morgan from  'morgan'
+import AuthRoutes from './routes/auth.routes.js'
 
 const app = express();
 
 app.use(morgan('dev'))
-app.use(express.static('public'))
+app.use(express.json())
+app.use('/api',AuthRoutes)
 
 export default app;
