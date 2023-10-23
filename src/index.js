7ed5380e6dd29d666b9a9632e67653cc45a1ba7a
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2023-10-23 14:02:08 -0600

    User Schema

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app.js'
 import { connectDB } from './db.js'
 
 const port = 4000;
-connectDB.conectTodDb()
-// connectDB();
+// connectDB.conectTodDb()
+connectDB();
 app.listen(port);
 console.log('Server on port '+port);
---
+++ src/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    username: {
+        type:String,
+        required: true,
+        trime: true
+    },
+    email:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type:String,
+        required: true
+    }
+})
+
+export default mongoose.model('User', userSchema)
